6d6a8a249995fbf110d4061be749aa495b20d77a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2023-04-22 15:32:34 -0400

    bit less logging

--- server/main.go
+++ server/main.go
@@ -25,7 +25,7 @@ func getSecretsFromEnv() {
 		}
 		*val = v
 		logf(ctx(), "Got %s, '%s'\n", key, v)
-		logf(ctx(), "Got %s\n", key)
+		// logf(ctx(), "Got %s\n", key)
 	}
 
 	getEnv("NOTED_AXIOM_TOKEN", &axiomApiToken, 40)
