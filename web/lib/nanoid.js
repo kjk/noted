30b1c27e929838b447a92af9ef9ef6486d2a6efd
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2023-04-28 19:01:02 -0400

    make nanoid() implementation simpler

--- web/lib/nanoid.js
+++ web/lib/nanoid.js
@@ -1,42 +1,30 @@
-// https://zelark.github.io/nano-id-cc/
-// this is pretty-printed https://esm.sh/v117/nanoid@4.0.2/es2022/nanoid.mjs
+// based on https://github.com/ai/nanoid/blob/main/non-secure/index.js
 
-var u = "useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict";
-var g = (t) => crypto.getRandomValues(new Uint8Array(t)),
-  h = (t, e, r) => {
-    let o = (2 << (Math.log(t.length - 1) / Math.LN2)) - 1,
-      n = -~((1.6 * o * e) / t.length);
-    return (s = e) => {
-      let l = "";
-      for (;;) {
-        let a = r(n),
-          p = n;
-        for (; p--; ) if (((l += t[a[p] & o] || ""), l.length === s)) return l;
-      }
-    };
-  },
-  i = (t, e = 21) => h(t, e, g),
-  c = (t = 21) =>
-    crypto
-      .getRandomValues(new Uint8Array(t))
-      .reduce(
-        (e, r) => (
-          (r &= 63),
-          r < 36
-            ? (e += r.toString(36))
-            : r < 62
-            ? (e += (r - 26).toString(36).toUpperCase())
-            : r > 62
-            ? (e += "-")
-            : (e += "_"),
-          e
-        ),
-        ""
-      );
-export {
-  i as customAlphabet,
-  h as customRandom,
-  c as nanoid,
-  g as random,
-  u as urlAlphabet,
+// urlAlphabet has 63 chars so we get size^63 random numbers
+// rough numbers:
+// 63 ^ 4 = 15,752,961 == 15.7 million
+// 63 ^ 5 = 992,436,543 == 992 million
+// 63 ^ 6 = 62,523,502,209 == 62 billion
+// 63 ^ 7 = 3,938,980,639,167== 3.9 trillion
+// 63 ^ 8 = 248,155,780,267,521 == 248 trillion
+
+// This alphabet uses `A-Za-z0-9_-` symbols.
+// The order of characters is optimized for better gzip and brotli compression.
+// References to the same file (works both for gzip and brotli):
+// `'use`, `andom`, and `rict'`
+// References to the brotli default dictionary:
+// `-26T`, `1983`, `40px`, `75px`, `bush`, `jack`, `mind`, `very`, and `wolf`
+
+let urlAlphabet =
+  "useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict";
+const nAlphabet = urlAlphabet.length;
+
+export let nanoid = (size = 21) => {
+  let rand = crypto.getRandomValues(new Uint8Array(size));
+  let id = "";
+  for (let i = 0; i < size; i++) {
+    let idx = rand[i] & nAlphabet;
+    id += urlAlphabet[idx];
+  }
+  return id;
 };
